ed6b9e860bfdd15fae0ec4766fb7e7bf7da0c9d4
Mattia <mattia.tinfena@gmail.com>
2024-10-25 10:01:52 +0200

    print points

--- lab2Main.py
+++ lab2Main.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 df = pd.read_csv("turkish-se-SP500vsMSCI.csv")
 
-print(df.columns)
+coordinates = []
+coordinates = df.columns
+
+row = df[coordinates[0]].tolist()
+col = df[coordinates[1]].tolist()
+
+plt.scatter(row, col) 
+plt.show() 
